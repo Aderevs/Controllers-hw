4f18dd3614dad5d715b4dfb71114d589e3c69ead
Aderevs <aderevs@gmail.com>
2024-04-01 17:10:44 +0300

    task3

---
+++ 001_SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _001_SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DownloadFile()
+        {
+            byte[] fileContent = System.IO.File.ReadAllBytes("App_Data/MVC.Middleware_8.0.zip");
+            return File(fileContent, "application/zip", "Lesson.zip");
+        }
+    }
+}
